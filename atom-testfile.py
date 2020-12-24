58ff153ee5808c4749ae94351b6c1b2ded41d9eb
Rob <raverker@xs4all.nl>
2020-12-24 21:15:12 +0100

    Started Python, variables

--- atom-testfile.py
+++ atom-testfile.py
@@ -1,6 +1,7 @@
-
+LineNumber = 1
 for kids in ["Caleb","Sydney","Savannah"]:
-    print ("Clean your room,", kids, "!")
+    print (LineNumber, "Clean your room,", kids, "!")
+    LineNumber = LineNumber + 1
 print ("test")
 my_input = input('Type your name: ')
 print (my_input)

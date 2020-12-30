fb66f4ea2a4d4fbcc41a541795f3d9cca9ecd6b5
Rob <raverker@xs4all.nl>
2020-12-30 14:59:50 +0100

    30-12-2020   RobV   substitute test

---
+++ learning_Python/substitute.py
@@ -0,0 +1,11 @@
+# testing functions to recreate perl substitute
+#
+# voorbeeld: org_string = 'testing'
+#            old_part = 'ing'
+#            new_part = 'en'
+# result wordt dan: 'testen'
+#
+# deze function pas alleen de eerste keer dat 'ie hem vindt aan
+#
+define substitute(org_string,old_part,new_part):
+      
